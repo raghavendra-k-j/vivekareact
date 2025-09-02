2019a0eeec5003978a79a4736f03f12830e7547b
Raghavendra K J <raghavendra.kj@bluecrimson.in>
2025-09-02 10:00:37 +0530

    Rearranged foder structure after completion of landing page

--- backupmodules/aigenbackup/components/InputSourceCommon.tsx
+++
@@ -1,9 +0,0 @@
-import { Card, CardBody } from "~/ui/components/card";
-
-export function InputSourceCard({ children }: { children: React.ReactNode }) {
-    return (<Card className="max-w-xl mx-auto w-full" shadow="sm" radius="sm">
-        <CardBody className="flex items-center">
-            {children}
-        </CardBody>
-    </Card>);
-}
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-import tsconfigPaths from "vite-tsconfig-paths"
+import react from '@vitejs/plugin-react'
 import path from 'path'
-import { visualizer } from "rollup-plugin-visualizer"
+import { defineConfig } from 'vite'
+import tsconfigPaths from "vite-tsconfig-paths"
 
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
     tailwindcss(),
-    visualizer({ open: true }),
   ],
   base: "/web",
   resolve: {
